c410dafcfd72792048ad7be416623590973939f3
Fernando Papito <eu@papito.io>
2019-03-02 10:47:18 -0300

    fixando as versoes usadas durante o curso

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'capybara'
-gem 'selenium-webdriver'
-gem 'rspec'
+gem 'capybara', ' 3.10.0'
+gem 'rspec', '3.8.0'
+gem 'selenium-webdriver', '3.14.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'capybara'
 require 'capybara/rspec'
+require 'selenium-webdriver'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -17,5 +18,5 @@ end
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.default_max_wait_time = 5
+  config.default_max_wait_time = 15
 end
