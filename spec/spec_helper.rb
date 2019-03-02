9c9e2a23e6f46c769f158db7b57507c482bdbbb2
Fernando Papito <eu@papito.io>
2019-03-02 12:07:59 -0300

    ajuste de logs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,15 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
+Capybara.register_driver :selenium_chrome do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--disable-site-isolation-trials'
+    # Sets the minimum log level. Valid values are from 0 to 3: INFO = 0, WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3.
+    opts.args << '--log-level=3'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.default_max_wait_time = 15
